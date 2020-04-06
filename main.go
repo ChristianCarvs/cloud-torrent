1837edc1369dea7a8e8c9d4afc6f2402dd94c2db
Chan2x <31362410+ChristianCarvs@users.noreply.github.com>
2020-04-06 08:01:39 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/jpillora/cloud-torrent/server"
+	"github.com/jpillora/opts"
+)
+
+var VERSION = "0.0.0-src" //set with ldflags
+
+func main() {
+	s := server.Server{
+		Title: "PRYERCarvs Cloud Server",
+		Port:  80,
+	}
+
+	o := opts.New(&s)
+	o.Version(VERSION)
+	o.Repo("https://github.com/ChristianCarvs/Cloud-Torrent")
+	o.PkgRepo()
+	o.SetLineWidth(96)
+	o.Parse()
+
+	if s.DisableLogTime {
+		log.SetFlags(0)
+	}
+
+	log.Printf("############# SimpleTorrent ver[%s] #############\n", VERSION)
+	if err := s.Run(VERSION); err != nil {
+		if errors.Is(err, server.ErrDiskSpace) {
+			log.Println(err)
+			os.Exit(42)
+		}
+		log.Fatal(err)
+	}
+}
